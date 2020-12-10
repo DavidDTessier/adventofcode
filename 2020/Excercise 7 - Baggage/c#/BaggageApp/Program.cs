665446b638766a7d8faadf322436d81437bcba7f
DuffMan2020 <57716152+DuffMan2020@users.noreply.github.com>
2020-12-10 10:20:47 -0500

    Day 8 solutio

---
+++ 2020/Excercise 7 - Baggage/c#/BaggageApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace BaggageApp
+{
+    class Program
+    {
+        const string myBag = "shiny gold";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Part 1: --");
+            var bags = LoadBags();
+            var count = 0;
+            bags.ToList().ForEach(b =>
+            {
+                if(ContainsBagColors(myBag, b.Key, bags))
+                {
+                    count++;
+                }
+
+            });
+
+            Console.WriteLine($"Count of bag colors that could contain {myBag} : {count}");
+
+            Console.WriteLine("Part 2: ---");
+            count = CountBags(myBag, bags);
+            Console.WriteLine($"Number of bag that {myBag} can contain: {count}");
+
+        }
+
+        static int CountBags(string bagName, IDictionary<string, Dictionary<string, int>> bags)
+        {
+            var count = 0;
+            foreach(var kv in bags[bagName])
+            {
+                count += kv.Value + kv.Value * (CountBags(kv.Key, bags));
+            }
+            return count;
+        }
+
+        static bool ContainsBagColors(string desiredBagColor, string bagName, IDictionary<string, Dictionary<string, int>> bags)
+        {
+            foreach( var kv in bags[bagName])
+            {
+                if (kv.Key.Equals(desiredBagColor))
+                {
+                    return true;
+                }
+                else if(ContainsBagColors(desiredBagColor, kv.Key, bags))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+
+        static IDictionary<string, Dictionary<string, int>> LoadBags()
+        {
+            var inputFilePath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location)
+                + Path.DirectorySeparatorChar + "input.dat";
+
+            var lines = File.ReadAllLines(inputFilePath);
+            var bags = new Dictionary<string, Dictionary<string, int>>();
+            lines.ToList().ForEach(l =>
+            {
+                var str = l.Split("contain");
+                string bag = str[0].Replace(" bags ", "");
+                var childBags = new Dictionary<string, int>();
+                var cbs = str[1].Split(",");
+                cbs.ToList().Where(s=>s.Trim() != "no other bags.").ToList().ForEach(cb =>
+                {
+                    var cn = int.Parse(Regex.Match(cb.Trim(), @"\d+").Value);
+                    var bag = Regex.Replace(cb.Trim(), @"\d+", "").Replace(".", "").Replace("bags","").Replace("bag","").Trim();
+                    childBags.Add(bag, cn);
+                });
+
+                bags.Add(bag,childBags);
+            });
+
+            return bags;
+        }
+    }
+}
+
---
+++ 2020/Excercise 8 - Handheld Halting/c#/HandheldHaltingApp/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace HandheldHaltingApp
+{
+    class Program
+    {
+        const string AccumulatorStep = "acc";
+        const string JumpStep = "jmp";
+        const string NoOpStep = "nop";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ins = LoadInstructions();
+       
+            var stepsRan = new List<int>();
+
+            
+            var flow = AccumulatorCount(0, 0, ins.ToList(), stepsRan);
+
+            Console.Write($"Accumulator Count Before Any Step Is Executed A Second Time: {flow.Accumulator}");
+            stepsRan.Clear();
+
+            flow = SwapActionsAndAccumulate(0, 0, ins.ToList(), stepsRan);
+
+            Console.Write($"Accumulator Count After the program terminates {flow.Accumulator}");
+        }
+
+        static InstructionFlow AccumulatorCount(int stepIndex,int accumCount, List<CodeInstruction> actions, List<int> stepsRan)
+        {
+
+            while (stepIndex != actions.Count)
+            {
+                var step = actions[stepIndex];
+
+                if (stepsRan.Contains(stepIndex))
+                    return new InstructionFlow { Accumulator = accumCount, InfiniteLoopHit = true };
+
+                stepsRan.Add(stepIndex);
+
+                if (step.Action == AccumulatorStep)
+                {
+                    accumCount += step.StepCount;
+                    stepIndex++;
+                }
+                else if (step.Action == JumpStep)
+                {
+                    stepIndex += step.StepCount;
+                }
+                else if (step.Action == NoOpStep)
+                {
+                    stepIndex++;
+                }
+
+
+            }
+
+            return new InstructionFlow { Accumulator = accumCount, InfiniteLoopHit = false };
+        }
+
+        static InstructionFlow SwapActionsAndAccumulate(int stepIndex, int accumCount, List<CodeInstruction> actions, List<int> stepsRan)
+        {
+
+            while (stepIndex != actions.Count)
+            {
+                var step = actions[stepIndex];
+
+                if (step.Action == AccumulatorStep)
+                {
+                    stepsRan.Add(stepIndex);
+                    accumCount += step.StepCount;
+                    stepIndex++;
+                }
+                else
+                {
+                    var swapStep = new CodeInstruction { Action = step.Action == JumpStep ? NoOpStep : JumpStep, StepCount = step.StepCount };
+                    actions[stepIndex] = swapStep;
+
+  
+                    var flow = AccumulatorCount(stepIndex, accumCount, actions, stepsRan.ConvertAll(idx => idx).ToList());
+
+                    if (flow.InfiniteLoopHit)
+                    {
+                        actions[stepIndex] = step;
+                        stepsRan.Add(stepIndex);
+                        stepIndex += step.Action == JumpStep ? step.StepCount : 1;
+                    }
+                    else
+                    {
+                        flow.FlowFixMessage = $"Replace Action {step.Action} with {swapStep.Action} at idx: {stepIndex} fixes operation flow.";
+                        return flow;
+                    }
+                }
+
+
+            }
+
+            return new InstructionFlow { Accumulator = accumCount, InfiniteLoopHit = false }; 
+        }
+
+      
+
+        static IList<CodeInstruction> LoadInstructions()
+        {
+            var inputFilePath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location)
+                + Path.DirectorySeparatorChar + "input.dat";
+
+            var instructions = File.ReadAllLines(inputFilePath).ToList().Select(l => 
+                new CodeInstruction { Action = l.Split(" ")[0].Trim(), StepCount = int.Parse(l.Split(" ")[1].Trim()) }
+            ).ToList();
+            return instructions;
+        }
+
+    }
+
+    class CodeInstruction
+    {
+        public string Action { get; set; }
+        public int StepCount { get; set; }
+    }
+
+    class InstructionFlow
+    {
+        public bool InfiniteLoopHit { get; set; }
+        public int Accumulator { get; set; }
+        public string FlowFixMessage { get; set; }
+    }
+}
