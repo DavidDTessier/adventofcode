dfb85af5838abbc2c47a968c30b8d6198ceb42ec
David Tessier <dtessier@lixar.com>
2021-01-05 09:34:55 -0500

    Updates

---
+++ 2020/Day 10 - Adapter Array/c#/AdapterArrayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdapterArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
